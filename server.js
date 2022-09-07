d23c8c7e70aa3b5b2a486e837ba932b5b173f0bb
TanyaGr9191 <tania91.gr@gmail.com>
2022-09-07 14:19:26 +0300

    backend heroku

--- server.js
+++ server.js
@@ -5,17 +5,30 @@ const app = express()
 
 app.use(cookieParser())
 app.use(express.static('public'))
+
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static('public'))
+}else{
+    const corsOptions ={
+        origin: "http://localhost:3002",
+        methods:"GET,PUT,POST,DELETE",
+        credentials: true
+    }
+    app.use(cors(corsOptions))
+}
+
 app.use(express.json())
-app.use(cors({
-    origin: "http://localhost:3002",
-    methods:"GET,PUT,POST,DELETE",
-    credentials: true
-}))
 
 // Express Routing:
 const toyRoutes = require('./api/toy/toy.controller')
 
 app.use('/api/toy', toyRoutes)
 
-app.listen(3030, () => console.log('Server ready at port 3030!'))
+app.get('/**', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+const port = process.env.PORT || 3030
+
+app.listen(port, () => console.log('Server ready at port:', port))
 
